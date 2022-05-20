fa2db1fab4a5ac452f2df7a8a27aea2aeafe0e44
M-Amer-Abutaleb <m.amer_abutaleb@hotmail.com>
2022-05-20 21:49:13 +0200

    initial commit

---
+++ src/Pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia aperiam esse, consectetur dolorum dolores provident est doloremque magnam explicabo? Debitis sapiente corporis veritatis at accusamus nesciunt possimus eum, magni dicta.</p>
+    </section>
+  )
+}
+
+export default About
